7923dc6e6d51fc76d4b1684a6aca5d9fd57b81ae
ksowah <sowahkelvin640@gmail.com>
2024-01-05 14:51:35 +0000

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by macbook on 05/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
